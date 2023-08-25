5e629b23f2bf9d2c2827efcbe51237f14ee838fb
Melchoirr <109651654+Melchoirr@users.noreply.github.com>
2023-08-25 15:10:32 +0800

    models

--- chat/models.py
+++ chat/models.py
@@ -15,15 +15,3 @@ class Chat(models.Model):
         db_table = 'Chat'
 
 
-class Message(models.Model):
-    image_path = models.CharField(max_length=255, blank=True, null=True)
-    file_path = models.IntegerField(blank=True, null=True)
-    type = models.IntegerField()
-    src_id = models.IntegerField()
-    dst_id = models.IntegerField()
-    chat_id = models.IntegerField(blank=True, null=True)
-    timestamp = models.DateTimeField(max_length=255, auto_now_add=True)
-
-    class Meta:
-        managed = True
-        db_table = 'Message'
