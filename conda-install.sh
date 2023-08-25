3c95c713424fa64e09a38ff2dc33c8e578d90901
Lord Turmoil <tony-turmoil@qq.com>
2023-08-25 15:09:32 +0800

    trivial: update

--- conda-install.sh
+++ conda-install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 conda install django==3.2.5
+condat install django-cors-headers
 conda install mysqlclient
 conda install djangorestframework-jwt
 conda install djangorestframework
 conda install channels
+install redis
+install celery
