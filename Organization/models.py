3d5c33084ba1f099ebe7c660901d25a2a3dc0ccd
Melchoirr <109651654+Melchoirr@users.noreply.github.com>
2023-08-25 09:29:01 +0800

    basic apps

--- Organization/models.py
+++ Organization/models.py
@@ -15,13 +15,3 @@ class Organization(models.Model):
         db_table = 'Organization'
 
 
-class UserOrganizationProfile(models.Model):
-    auth = models.IntegerField(blank=True, null=True)
-    user_id = models.IntegerField(primary_key=True)
-    org_id = models.IntegerField()
-    nickname = models.CharField(max_length=63)
-
-    class Meta:
-        managed = True
-        db_table = 'UserOrganizationProfile'
-        unique_together = (('user_id', 'org_id'),)
