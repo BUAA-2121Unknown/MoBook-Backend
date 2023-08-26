be452db187c196d72773ceb1c8f17cd3d4a6a7de
Melchoirr <109651654+Melchoirr@users.noreply.github.com>
2023-08-26 10:08:51 +0800

    solved

--- MoBook/urls.py
+++ MoBook/urls.py
@@ -23,6 +23,6 @@ from MoBook.settings import MEDIA_URL, MEDIA_ROOT
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('api/oauth/', include(('oauth.urls', 'oauth'))),
-                  path('api/chat/', include(('chat.urls', 'chat')))
+                  path('api/chat/', include(('chat.urls', 'chat'))),
                   path('api/user/', include(('user.urls', 'user'))),
               ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
