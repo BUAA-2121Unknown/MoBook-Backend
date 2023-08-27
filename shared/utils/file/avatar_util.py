a0f6d0e6d04550fb5c64be9b12865828f1abfdf4
Lord Turmoil <tony-turmoil@qq.com>
2023-08-28 02:04:44 +0800

    fix: upload avatar bug

--- shared/utils/file/avatar_util.py
+++ shared/utils/file/avatar_util.py
@@ -11,11 +11,12 @@ from shared.utils.dir_utils import ensure_avatar_path
 
 def save_avatar(typ, old_path, new_path, file):
     ensure_avatar_path(typ)
-    if old_path:
+    if old_path and os.path.exists(old_path):
         try:
             os.remove(old_path)
-        except Exception as e:
-            raise e
+        except Exception:
+            # shouldn't raise exception! swallow it!
+            pass
 
     try:
         f = open(new_path, "wb")
