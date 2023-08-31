17e343125d7f141c28e0b5760151db910cd8cfee
Lord Turmoil <tony-turmoil@qq.com>
2023-08-31 12:42:33 +0800

    feat: add cache util

--- shared/utils/cache/cache_utils.py
+++ shared/utils/cache/cache_utils.py
@@ -4,8 +4,31 @@
 # @Author  : Tony Skywalker
 # @File    : cache_utils.py
 #
-from user.models import User
+import pickle
+
+from django.core.cache import cache
+
+from shared.utils.model.model_extension import first_or_default
 
 
-def first_or_default_by_cache(model, _id):
-    model.Meta.
+def get_cache_key(model, pk):
+    return model.Meta.verbose_name + ":" + str(pk)
+
+
+def first_or_default_by_cache(model, pk):
+    key = get_cache_key(model, pk)
+    pickled_obj = cache.get(key)
+    if pickled_obj is None:
+        obj = first_or_default(model, pk=pk)
+        if obj is not None:
+            cache.set(key, pickle.dumps(obj))
+    else:
+        obj = pickle.loads(pickled_obj)
+
+    return key, obj
+
+
+def update_cache(model, pk, obj):
+    key = get_cache_key(model, pk)
+    cache.set(key, pickle.dumps(obj))
+    return key
