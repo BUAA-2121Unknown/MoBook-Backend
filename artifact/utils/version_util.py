6ebfdb8452e9df5cddcfa5143f48fabc6118c648
Lord Turmoil <tony-turmoil@qq.com>
2023-09-02 08:53:55 +0800

    fix: sort file version by updated time

--- artifact/utils/version_util.py
+++ artifact/utils/version_util.py
@@ -8,4 +8,4 @@ from artifact.models import Item, FileVersion
 
 
 def get_versions_of_file(file: Item):
-    return FileVersion.objects.filter(file_id=file.id).order_by('-version')
+    return FileVersion.objects.filter(file_id=file.id).order_by('-updated')
